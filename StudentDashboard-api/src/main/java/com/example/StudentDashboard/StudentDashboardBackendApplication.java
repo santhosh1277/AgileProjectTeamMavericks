cdee1f08d92131ab9304529cc2d2faafd3a86a40
santhosh1277 <santhoshreddy8317@gmail.com>
2025-10-27 20:12:31 +0000

    MAV-20
    Added spring boot  project for backend

---
+++ StudentDashboard-api/src/main/java/com/example/StudentDashboard/StudentDashboardBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.StudentDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDashboardBackendApplication.class, args);
+		System.out.println("Hello Studnet");
+	}
+
+}
---
+++ StudentDashboard-api/src/test/java/com/example/StudentDashboard/StudentDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
