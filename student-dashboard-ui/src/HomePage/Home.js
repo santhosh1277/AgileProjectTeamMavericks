c05b5c0695eac1ade0bc94263fa567fa44853a7a
santhosh1277 <santhoshreddy8317@gmail.com>
2025-10-30 20:11:07 +0000

    Mav-3
    Added code for Navbar

--- student-dashboard-ui/src/HomePage/Home.js
+++ student-dashboard-ui/src/HomePage/Home.js
@@ -1,9 +1,23 @@
 function Home() {
-    return (
-        <div className="container-fluid" >
-            <div className="row bg-dark">
-                <div className="col-12 text-center"><h1 className="text-light text-align-center">Welcome to Student Dashboard</h1></div>
-            </div>
-        </div>);
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+    >
+      <h1 style={{ color: "white", textAlign: "center", margin: 0, padding: 0 }}>
+        Welcome to Student Dashboard
+      </h1>
+    </div>
+  );
 }
+
 export default Home;
